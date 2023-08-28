f2a2f74e27ab82cc0ca65a9c932c313ad444e0d4
Cameron Angliss <cameronangliss99@gmail.com>
2023-08-28 16:59:17 -0500

    train until it's good

--- homework1/homework/train.py
+++ homework1/homework/train.py
@@ -16,7 +16,7 @@ def train(args):
     valid_data = load_data("data/valid")
 
     # Run SGD for several epochs
-    for _ in range(10):
+    while True:
         for batch in train_data:
             inputs = batch[0]
             labels = batch[1]
@@ -25,8 +25,8 @@ def train(args):
             optimizer.zero_grad()
             error.backward()
             optimizer.step()
-        # if accuracy(outputs, labels) > 0.9:
-        #     break
+        if accuracy(outputs, labels) > 0.9:
+            break
 
     # Save your final model, using save_model
     save_model(model)
