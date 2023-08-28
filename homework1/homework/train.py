2e7ed31d0a31d101bba257ebe9508d46b5479f54
Cameron Angliss <cameronangliss99@gmail.com>
2023-08-28 17:17:53 -0500

    lowering epochs

--- homework1/homework/train.py
+++ homework1/homework/train.py
@@ -16,7 +16,7 @@ def train(args):
     valid_data = load_data("data/valid")
 
     # Run SGD for several epochs
-    for _ in range(10):
+    for _ in range(5):
         for batch in train_data:
             inputs = batch[0]
             labels = batch[1]
