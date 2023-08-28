ed98f929fc08954e8c970f881c380111631d24fb
Cameron Angliss <cameronangliss99@gmail.com>
2023-08-28 18:16:21 -0500

    fix

--- homework1/homework/train.py
+++ homework1/homework/train.py
@@ -19,7 +19,7 @@ def train(args):
         for batch in train_data:
             inputs = batch[0].to(model.device)
             labels = batch[1]
-            outputs = model.forward(inputs).to(model.device)
+            outputs = model.forward(inputs)
             error = loss.forward(outputs, labels)
             optimizer.zero_grad()
             error.backward()
