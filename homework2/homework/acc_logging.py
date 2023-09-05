3b2a1dfcaa64b1ba95ee8f7ca0a8cbfcffe52254
Cameron Angliss <cameronangliss99@gmail.com>
2023-09-05 09:33:42 -0500

    trying this

--- homework2/homework/acc_logging.py
+++ homework2/homework/acc_logging.py
@@ -20,14 +20,14 @@ def test_logging(train_logger: tb.SummaryWriter, valid_logger: tb.SummaryWriter)
         for iteration in range(20):
             dummy_train_loss = 0.9**(epoch+iteration/20.)
             dummy_train_accuracy = epoch/10. + torch.randn(10)
-            train_logger.add_scalar('train/loss', dummy_train_loss, global_step=global_step)
+            train_logger.add_scalar('loss', dummy_train_loss, global_step=global_step)
             global_step += 1
-        train_logger.add_scalar('train/accuracy', torch.mean(dummy_train_accuracy), global_step=global_step)
+        train_logger.add_scalar('accuracy', torch.mean(dummy_train_accuracy), global_step=global_step)
         torch.manual_seed(epoch)
         for iteration in range(10):
             dummy_validation_accuracy = epoch / 10. + torch.randn(10)
             global_step += 1
-        valid_logger.add_scalar('valid/accuracy', torch.mean(dummy_validation_accuracy), global_step=global_step)
+        valid_logger.add_scalar('accuracy', torch.mean(dummy_validation_accuracy), global_step=global_step)
 
 
 if __name__ == "__main__":
