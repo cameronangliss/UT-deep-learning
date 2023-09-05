6e013ed7dba42b9d2e5a27ff87ff30d86e623da8
Cameron Angliss <cameronangliss99@gmail.com>
2023-09-05 11:15:08 -0500

    undo

--- homework2/homework/train.py
+++ homework2/homework/train.py
@@ -25,7 +25,6 @@ def train(args):
     # Run SGD for several epochs
     while True:
         for batch in train_data:
-            print(batch)
             inputs = batch[0].to(device)
             labels = batch[1].to(device)
             outputs = model.forward(inputs)
