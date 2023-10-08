36635af15229613294a7d1e73d6b239ce6fadb49
Cameron Angliss <cameronangliss99@gmail.com>
2023-10-08 01:42:26 -0500

    upping expectation

--- homework3/homework/train_cnn.py
+++ homework3/homework/train_cnn.py
@@ -62,7 +62,7 @@ def train(args):
             n += 1
         score /= n
         valid_logger.add_scalar('accuracy', score, global_step=global_step)
-        if score > 0.9:
+        if score > 0.92:
             break
 
     # Save your final model, using save_model
