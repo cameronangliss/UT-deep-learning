1202e6a5e3e75724e957dc2f09639c322769a10c
Cameron Angliss <cameronangliss99@gmail.com>
2023-11-02 01:48:26 -0500

    copied HW4 training code

--- homework5/homework/train.py
+++ homework5/homework/train.py
@@ -7,15 +7,61 @@ from . import dense_transforms
 
 def train(args):
     from os import path
-    model = Planner()
     train_logger, valid_logger = None, None
     if args.log_dir is not None:
         train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'))
 
-    """
-    Your code here, modify your HW4 code
-    Hint: Use the log function below to debug and visualize your model
-    """
+    # create a model, loss, optimizer
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = Planner().to(device)
+    # model.load_state_dict(torch.load("homework/det.th"))
+    loss = torch.nn.BCEWithLogitsLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, weight_decay=1e-4)
+
+    # load the data: train and valid
+    train_transform = dense_transforms.Compose(
+        [
+            dense_transforms.ColorJitter(
+                brightness=0.5, contrast=0.5, saturation=0.5, hue=0.25
+            ),
+            dense_transforms.RandomHorizontalFlip(),
+            dense_transforms.ToTensor(),
+            dense_transforms.ToHeatmap(),
+        ]
+    )
+    valid_transform = dense_transforms.Compose(
+        [
+            dense_transforms.ToTensor(),
+            dense_transforms.ToHeatmap(),
+        ]
+    )
+    train_data = load_data("dense_data/train", transform=train_transform)
+    valid_data = load_data("dense_data/valid", transform=valid_transform)
+
+    # Run SGD for several epochs
+    global_step = 0
+    for _ in range(50):
+        for batch in train_data:
+            images = batch[0].to(device)
+            heatmaps = batch[1].to(device)
+            model_output = model.forward(images)
+            log(train_logger, images, heatmaps, model_output, global_step)
+            train_error = loss.forward(model_output, heatmaps)
+            train_logger.add_scalar("loss", train_error, global_step=global_step)
+            optimizer.zero_grad()
+            train_error.backward()
+            optimizer.step()
+            global_step += 1
+        print("training error:", train_error.item())
+        avg_error = 0
+        i = 0
+        for batch in valid_data:
+            images = batch[0].to(device)
+            heatmaps = batch[1].to(device)
+            valid_error = loss.forward(model_output, heatmaps)
+            i += 1
+            avg_error += (1 / i) * (valid_error - avg_error)
+        print("validation error:", avg_error.item())
 
     save_model(model)
 
