5e538f80768188b46ee748f3ea14532ff04eff9e
Cameron Angliss <cameronangliss99@gmail.com>
2023-11-02 13:11:28 -0500

    trying to fix issue

--- homework5/homework/controller.py
+++ homework5/homework/controller.py
@@ -1,5 +1,7 @@
 import pystk
 
+from .utils import PyTux
+
 
 def control(aim_point, current_vel):
     """
@@ -28,13 +30,13 @@ def control(aim_point, current_vel):
 
 def test_controller(pytux, track, verbose):
     import numpy as np
+    pytux = PyTux()
     steps, how_far = pytux.rollout(track, control, max_frames=1000, verbose=verbose)
     print(steps, how_far)
     pytux.close()
 
 
 if __name__ == '__main__':
-    from .utils import PyTux
     from argparse import ArgumentParser
 
     parser = ArgumentParser()
