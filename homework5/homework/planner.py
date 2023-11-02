7bf5d06707c76d524f4a3fbbdb5c6c2ecfb0d5eb
Cameron Angliss <cameronangliss99@gmail.com>
2023-11-02 11:37:10 -0500

    copied FCN from HW4

--- homework5/homework/planner.py
+++ homework5/homework/planner.py
@@ -14,22 +14,84 @@ def spatial_argmax(logit):
 
 
 class Planner(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
+    class Block(torch.nn.Module):
+        def __init__(self, n_input, n_output, stride=1):
+            super().__init__()
+            self.n_input = n_input
+            self.n_output = n_output
+            self.layers = torch.nn.Sequential(
+                torch.nn.Conv2d(n_input, n_output, kernel_size=3, padding=1, stride=stride),
+                torch.nn.BatchNorm2d(n_output),
+                torch.nn.ReLU(),
+            )
+            self.downsample = torch.nn.Sequential(
+                torch.nn.Conv2d(n_input, n_output, kernel_size=1, stride=stride),
+                torch.nn.BatchNorm2d(n_output)
+            )
 
+        def forward(self, x):
+            if x.size()[0] == x.size()[2] == x.size()[3] == 1:
+                if self.n_output > self.n_input:
+                    zeros = torch.zeros(1, self.n_output - self.n_input, 1, 1)
+                    return torch.cat([zeros, x], dim=1)
+                else:
+                    return x[:, :self.n_output, :, :]
+            else:
+                return self.layers(x) + self.downsample(x)
+
+    def __init__(self, layers=[32, 64, 128, 256], n_input_channels=3):
         """
         Your code here
         """
-        raise NotImplementedError('Planner.__init__')
 
-    def forward(self, img):
+        super().__init__()
+        self.down_blocks = []
+        c = n_input_channels
+        for l in layers:
+            self.down_blocks.append(self.Block(c, l))
+            c = l
+        self.pool = torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.up_convs = []
+        for l in reversed(layers[1:]):
+            self.up_convs.append(torch.nn.ConvTranspose2d(l, l, kernel_size=3, stride=2, padding=1, output_padding=1))
+        self.up_blocks = []
+        rev_layers = list(reversed(layers))
+        for i in range(len(layers) - 1):
+            self.up_blocks.append(self.Block(rev_layers[i] + rev_layers[i + 1], rev_layers[i + 1]))
+        self.final_conv = torch.nn.Conv2d(layers[0], 3, kernel_size=1)
+        self.network_chain = torch.nn.Sequential(*self.down_blocks, *self.up_convs, *self.up_blocks)
+
+    def forward(self, x):
         """
         Your code here
         Predict the aim point in image coordinate, given the supertuxkart image
         @img: (B,3,96,128)
         return (B,2)
         """
-        raise NotImplementedError("Planner.forward")
+
+        activations = []
+        # print("start", x.size())
+        for block in self.down_blocks[:-1]:
+            x = block(x)
+            # print("side", x.size())
+            activations.append(x)
+            x = self.pool(x)
+            # print("down", x.size())
+        x = self.down_blocks[-1](x)
+        # print("side", x.size())
+        rev_acts = list(reversed(activations))
+        for i in range(len(self.up_blocks)):
+            x = self.up_convs[i](x)
+            # print("up", x.size())
+            H = rev_acts[i].size()[2]
+            W = rev_acts[i].size()[3]
+            x = torch.cat([x[:, :, :H, :W], rev_acts[i]], dim=1)
+            # print("cat", x.size())
+            x = self.up_blocks[i](x)
+            # print("side", x.size())
+        x = self.final_conv(x)
+        # print("final", x.size())
+        return x
 
 
 def save_model(model):
