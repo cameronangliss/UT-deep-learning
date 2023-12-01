b62883ce7d8f045678dc35b1c58fec886c851725
Cameron Angliss <cameronangliss99@gmail.com>
2023-12-01 02:07:06 -0600

    indicating if puck cannot be found

--- final/image_agent/detector.py
+++ final/image_agent/detector.py
@@ -9,7 +9,9 @@ def spatial_argmax(logit):
     :return: A tensor of size BS x 2 the soft-argmax in normalized coordinates (-1 .. 1)
     """
     weights = F.softmax(logit.view(logit.size(0), -1), dim=-1).view_as(logit)
-    print(torch.max(weights))
+    # indicating if the puck is not seen
+    if torch.max(weights) < 0.01:
+        return None
     return torch.stack(((weights.sum(1) * torch.linspace(-1, 1, logit.size(2)).to(logit.device)[None]).sum(1),
                         (weights.sum(2) * torch.linspace(-1, 1, logit.size(1)).to(logit.device)[None]).sum(1)), 1)
 
@@ -96,8 +98,11 @@ class Detector(torch.nn.Module):
 
     def detect(self, x):
         x = self.forward(x[None])
-        return spatial_argmax(x)[0]
-
+        detections = spatial_argmax(x)
+        if detections is None:
+            return None
+        else:
+            return detections[0]
 
 
 def save_model(model):
