58c934f9c2713f80e8e71ef3aa2f8ff08bb25efe
Rabbittroll <pbmodonnell@utexas.edu>
2023-12-01 18:38:22 -0500

    j

--- final/image_agent/player.py
+++ final/image_agent/player.py
@@ -178,7 +178,7 @@ class Team:
                 steer = 0
                 drift = False
                 
-                if (17 <= self.act_count <= 17):
+                if (18 <= self.act_count <= 18):
                     steer = 1
 
             action = dict(
