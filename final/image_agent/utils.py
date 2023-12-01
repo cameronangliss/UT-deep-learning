f1d644b6c5fcf6486e3bb7a6197643b504ccb3ab
Cameron Angliss <cameronangliss99@gmail.com>
2023-11-30 23:53:56 -0600

    debugging

--- final/image_agent/utils.py
+++ final/image_agent/utils.py
@@ -20,6 +20,7 @@ class SuperTuxDataset(Dataset):
             img = Image.open(f.replace('.csv', '.png'))
             img.load()
             label = np.loadtxt(f, dtype=np.float32, delimiter=',')
+            print(label)
             formatted_label = np.concatenate(label, [0, 0]).reshape(1, -1)
             self.data.append((img, formatted_label))
         self.transform = transform
