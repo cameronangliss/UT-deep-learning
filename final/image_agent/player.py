9d285a0ccb4759821467b8444933df26537edb91
Rabbittroll <pbmodonnell@utexas.edu>
2023-12-01 19:24:57 -0500

    f

--- final/image_agent/player.py
+++ final/image_agent/player.py
@@ -171,7 +171,7 @@ class Team:
             drift=abs(steer) > 0.7
              
             #immediate behavior        
-            if (self.act_count < 300):
+            if (self.act_count < 100):
                 nitro = True
                 acceleration = 1
                 fire = True
