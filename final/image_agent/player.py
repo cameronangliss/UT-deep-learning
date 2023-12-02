e389c91812f4a4a1e937ba9e7e75c6430b3282b9
Rabbittroll <pbmodonnell@utexas.edu>
2023-12-02 00:47:37 -0500

    e

--- final/image_agent/player.py
+++ final/image_agent/player.py
@@ -178,7 +178,7 @@ class Team:
                     steer = 1
                     drift = True
                     
-                if (80 <= self.act_count <= 81):
+                if (80 <= self.act_count <= 80):
                     steer = 1
                     drift = True
                     
