7f3f2c505be13bcdc5f3c6d3866dc2208a13236c
Cameron Angliss <cameronangliss99@gmail.com>
2023-12-02 15:35:34 -0600

    testing

--- final/image_agent/detector.py
+++ final/image_agent/detector.py
@@ -110,7 +110,7 @@ class CNNClassifier(torch.nn.Module):
         """
       
         
-        layers=[16, 32, 64]
+        layers=[16, 32, 64, 128]
         n_input_channels=3
         n_output_channels=1
         stride=2
