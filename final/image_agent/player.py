c2288bc927f7d66f3fa4170e7b71455043bac791
Rabbittroll <pbmodonnell@utexas.edu>
2023-12-01 20:02:27 -0500

    l

--- final/image_agent/player.py
+++ final/image_agent/player.py
@@ -187,7 +187,7 @@ class Team:
                 if (self.act_count < 70):
                     acceleration = 0
                     
-                if (70 <= self.act_count <= 93):
+                if (70 <= self.act_count <= 91):
                     steer = 1
                 
 
