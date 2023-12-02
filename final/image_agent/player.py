4ddaed85d20b0db306b108f73fd96b52aad53aeb
Cameron Angliss <cameronangliss99@gmail.com>
2023-12-02 13:54:17 -0600

    drive slower

--- final/image_agent/player.py
+++ final/image_agent/player.py
@@ -101,9 +101,9 @@ class Team:
                 self.frame = 1
 
             # setting values for normal behavior (may be changed by later code for edge cases)
-            if np.linalg.norm(player_state[i]["kart"]["velocity"]) < 10:
+            if np.linalg.norm(player_state[i]["kart"]["velocity"]) < 7:
                     acceleration = 0.5
-            elif np.linalg.norm(player_state[i]["kart"]["velocity"]) > 15:
+            elif np.linalg.norm(player_state[i]["kart"]["velocity"]) > 12:
                 acceleration = 0
                 brake = True
             else:
